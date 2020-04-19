1b06d78f37af8f30aec22f3267a3d09cd0b75f5c
Mahesh <Parvekar.mahesh@gmail.com>
2020-04-19 18:19:13 +0530

    Displaying data in component

--- src/app/messages.component.ts
+++ src/app/messages.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core'
 @Component({
 selector: 'messages',
-template: 'this is a message template.. it will be shown in app componennrt html' 
+template: 'this is a message component.The mumber of messges are : {{messages.length}}  The first message is {{messages[0].text}} owner is {{messages[0].owner}} '
+
+
 
 })
 
 export class MessagesComponent
 {
-
+messages= [{text:'text1', owner:'Mahesh'}, {text:'text2', owner:'Umesh'}];
 }
